567c58959ff687eaf1891ae0145c8373268409f5
ShawCole <shaw.cole@bizypro.co>
2025-03-23 22:00:36 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -88,7 +88,7 @@ function App() {
                 <img
                   src="/logo.png"
                   alt="Company Logo"
-                  className="w-200 h-auto"
+                  className="w-70 h-auto"
                 />
               </div>
 
